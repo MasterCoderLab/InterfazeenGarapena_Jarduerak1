c39398654c4db6eca8b5e0c02900c07da6b506c2
Ion Beristain <jonboyka@gmail.com>
2026-09-14 10:19:29 +0200

    refactor: erabili Oparia klasea I01_03 jardueran

--- I01_03_OpariLista/I01_03_OpariLista/MainPage.xaml.cs
+++ I01_03_OpariLista/I01_03_OpariLista/MainPage.xaml.cs
@@ -1,96 +1,126 @@
-﻿namespace I01_03_OpariLista;
-
-public partial class MainPage : ContentPage
+﻿namespace I01_03_OpariLista
 {
-    private string? aukeratutakoOparia;
-    private string? lehenOparia;
-    private string? bigarrenOparia;
-
-    public MainPage()
+    // Opari bakoitzaren datuak gordetzeko klasea
+    public class Oparia
     {
-        InitializeComponent();
+        public string izena;
+        public string irudia;
+
+        public Oparia(string izena, string irudia)
+        {
+            this.izena = izena;
+            this.irudia = irudia;
+        }
+
+        // ListView-ean opariaren izena erakusteko
+        public override string ToString()
+        {
+            return izena;
+        }
     }
 
-    private void OpariListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    public partial class MainPage : ContentPage
     {
-        if (e.SelectedItem == null)
+        // Opari guztiak gordetzeko array-a
+        private Oparia[] opariak;
+
+        // Aukeratutako opariak
+        private Oparia? aukeratutakoOparia;
+        private Oparia? lehenOparia;
+        private Oparia? bigarrenOparia;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Opari objektuak sortu
+            opariak = new Oparia[]
+            {
+                new Oparia("Erlojua", "erlojua.png"),
+                new Oparia("Aurikularrak", "aurikularrak.png"),
+                new Oparia("Bozgorailuak", "bozgorailuak.png"),
+                new Oparia("Ermangarria", "eramangarria.png"),
+                new Oparia("Bizikleta elektrikoa", "bizikleta.png")
+            };
+
+            // Opariak ListView-ean erakutsi
+            OpariListView.ItemsSource = opariak;
+        }
+
+        // Listako opari bat aukeratzean
+        private void OpariListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            // Ez badago ezer aukeratuta
+            if (e.SelectedItem == null)
+            {
+                aukeratutakoOparia = null;
+                OpariImage.Source = null;
+                AukeratutakoOpariaLabel.Text = "";
+                return;
+            }
+
+            // Aukeratutako objektua Oparia motara bihurtu
+            aukeratutakoOparia = (Oparia)e.SelectedItem;
+
+            // Opariaren izena eta irudia erakutsi
+            AukeratutakoOpariaLabel.Text = aukeratutakoOparia.izena;
+            OpariImage.Source = aukeratutakoOparia.irudia;
+        }
+
+        // Aukeratu botoia sakatzean
+        private void AukeratuButton_Clicked(object sender, EventArgs e)
+        {
+            // Ez badago oparirik aukeratuta
+            if (aukeratutakoOparia == null)
+            {
+                return;
+            }
+
+            // Lehen oparia hutsik badago
+            if (lehenOparia == null)
+            {
+                lehenOparia = aukeratutakoOparia;
+                LehenOpariaLabel.Text = lehenOparia.izena;
+            }
+
+            // Lehen oparia badago, bigarrena ezarri
+            else if (bigarrenOparia == null)
+            {
+                bigarrenOparia = aukeratutakoOparia;
+                BigarrenOpariaLabel.Text = bigarrenOparia.izena;
+
+                // Ezin dira bi opari baino gehiago aukeratu
+                AukeratuButton.IsEnabled = false;
+            }
+        }
+
+        // Dena garbitzeko
+        private void EzabatuButton_Clicked(object sender, EventArgs e)
         {
             aukeratutakoOparia = null;
+            lehenOparia = null;
+            bigarrenOparia = null;
+
+            // ListView-ko aukeraketa kendu
+            OpariListView.SelectedItem = null;
+
+            // Irudia eta testuak garbitu
             OpariImage.Source = null;
             AukeratutakoOpariaLabel.Text = "";
-            return;
+            LehenOpariaLabel.Text = "";
+            BigarrenOpariaLabel.Text = "";
+
+            // Aukeratu botoia berriro aktibatu
+            AukeratuButton.IsEnabled = true;
         }
 
-        aukeratutakoOparia = e.SelectedItem.ToString();
-        AukeratutakoOpariaLabel.Text = aukeratutakoOparia;
-
-        switch (aukeratutakoOparia)
+        // Aplikaziotik irteteko
+        private void IrtenButton_Clicked(object sender, EventArgs e)
         {
-            case "Erlojua":
-                OpariImage.Source = "erlojua.png";
-                break;
-
-            case "Aurikularrak":
-                OpariImage.Source = "aurikularrak.png";
-                break;
-
-            case "Bozgorailuak":
-                OpariImage.Source = "bozgorailuak.png";
-                break;
-
-            case "Eramangarria":
-                OpariImage.Source = "eramangarria.png";
-                break;
-
-            case "Bizikleta elektrikoa":
-                OpariImage.Source = "bizikleta.png";
-                break;
-        }
-    }
-
-    private void AukeratuButton_Clicked(object sender, EventArgs e)
-    {
-        if (aukeratutakoOparia == null)
-        {
-            return;
-        }
-
-        if (lehenOparia == null)
-        {
-            lehenOparia = aukeratutakoOparia;
-            LehenOpariaLabel.Text = lehenOparia;
-        }
-        else if (bigarrenOparia == null)
-        {
-            bigarrenOparia = aukeratutakoOparia;
-            BigarrenOpariaLabel.Text = bigarrenOparia;
-
-            AukeratuButton.IsEnabled = false;
-        }
-    }
-
-    private void EzabatuButton_Clicked(object sender, EventArgs e)
-    {
-        aukeratutakoOparia = null;
-        lehenOparia = null;
-        bigarrenOparia = null;
-
-        OpariListView.SelectedItem = null;
-
-        OpariImage.Source = null;
-        AukeratutakoOpariaLabel.Text = "";
-
-        LehenOpariaLabel.Text = "";
-        BigarrenOpariaLabel.Text = "";
-
-        AukeratuButton.IsEnabled = true;
-    }
-
-    private void IrtenButton_Clicked(object sender, EventArgs e)
-    {
-        if (Window != null)
-        {
-            Application.Current?.CloseWindow(Window);
+            if (Window != null)
+            {
+                Application.Current?.CloseWindow(Window);
+            }
         }
     }
 }
