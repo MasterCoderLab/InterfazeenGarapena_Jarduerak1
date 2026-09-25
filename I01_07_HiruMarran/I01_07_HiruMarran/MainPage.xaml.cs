2556115084b17c95810a23413711e52821796bdb
Ion Beristain <jonboyka@gmail.com>
2026-09-25 11:59:09 +0200

    feat: gehitu I01_07 jokalari txandak eta X O irudiak

--- I01_07_HiruMarran/I01_07_HiruMarran/MainPage.xaml.cs
+++ I01_07_HiruMarran/I01_07_HiruMarran/MainPage.xaml.cs
@@ -1,24 +1,75 @@
-﻿namespace I01_07_HiruMarran
+﻿namespace I01_07_HiruMarran;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    // Taularen egoera gordetzeko
+    private string[,] taula = new string[3, 3];
+
+    // Uneko jokalaria
+    private string jokalaria = "X";
+
+
+    public MainPage()
     {
-        int count = 0;
+        InitializeComponent();
+    }
 
-        public MainPage()
+
+    // Taulako laukiren bat sakatzen denean
+    private void Casilla_Clicked(object? sender, EventArgs e)
+    {
+        ImageButton casilla = (ImageButton)sender;
+
+        string posizioa = casilla.CommandParameter.ToString();
+
+        string[] datuak = posizioa.Split(',');
+
+        int fila = int.Parse(datuak[0]);
+        int columna = int.Parse(datuak[1]);
+
+
+        // Laukia hutsik badago bakarrik jolastu
+        if (string.IsNullOrEmpty(taula[fila, columna]))
         {
-            InitializeComponent();
-        }
+            taula[fila, columna] = jokalaria;
 
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
+            if (jokalaria == "X")
+            {
+                casilla.Source = "x.png";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+                jokalaria = "O";
+                TxandaIkurraLabel.Text = "⭕";
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
+            {
+                casilla.Source = "o.png";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                jokalaria = "X";
+                TxandaIkurraLabel.Text = "❌";
+            }
         }
+    }
+
+
+    // Partida berriro hasteko
+    private void BerriroHasi_Clicked(object? sender, EventArgs e)
+    {
+        taula = new string[3, 3];
+
+        jokalaria = "X";
+
+        TxandaIkurraLabel.Text = "❌";
+
+        Casilla00.Source = null;
+        Casilla01.Source = null;
+        Casilla02.Source = null;
+
+        Casilla10.Source = null;
+        Casilla11.Source = null;
+        Casilla12.Source = null;
+
+        Casilla20.Source = null;
+        Casilla21.Source = null;
+        Casilla22.Source = null;
     }
 }
